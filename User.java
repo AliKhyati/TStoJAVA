4fc32a286147990fc6cf25f8d71c9ff445e69857
alikhyati <alikhyatihotmail.com>
2022-09-05 14:04:16 +0200

    first commit

---
+++ User.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class User {
+    private Date birthday;
+    private Integer size;
+    private ArrayList<Photo> photos;
+    private Address address;
+}
